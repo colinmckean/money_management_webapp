3eea2daf068399035a5a6e8b4843cabba3b0cbd2
Colin McKean <colinmckeandigital@gmail.com>
2017-02-04 10:21:32 +0000

    create database and tables, confirm in psql database and tables are created.

---
+++ db/moneycashboard.sql
@@ -0,0 +1,21 @@
+DROP TABLE tags CASCADE;
+DROP TABLE merchants CASCADE;
+DROP TABLE transactions CASCADE;
+
+CREATE TABLE tags(
+id SERIAL8 primary key,
+tag_name VARCHAR(255)
+);
+
+CREATE TABLE merchants(
+id SERIAL8 primary key,
+merchant_name VARCHAR(255)
+);
+
+CREATE TABLE transactions(
+id SERIAL8 primary key,
+tag_id INT4 references tags(id),
+merchant_id INT4 references merchants(id),
+transaction_amount REAL,
+item_description TEXT
+);
