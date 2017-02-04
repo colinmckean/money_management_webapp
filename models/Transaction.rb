285f6f8ccfcba1020b34f3b2296a4dd1d2d610d2
Colin McKean <colinmckeandigital@gmail.com>
2017-02-04 12:29:51 +0000

    add initialize and save function to Transaction model

---
+++ models/Transaction.rb
@@ -0,0 +1,20 @@
+require_relative '../db/sql_runner'
+
+class Transaction
+  attr_reader :id, :merchant_id,:tag_id, :transaction_amount, :item_description
+  def initialize(options)
+    @id = options['id'].to_i
+    @merchant_id = options['merchant_id'].to_i
+    @tag_id = options['tag_id'].to_i
+    @transaction_amount = options['transaction_amount'].to_f
+    @item_description = options['item_description']
+  end
+
+  def save()
+    sql = ("INSERT INTO transactions(merchant_id, tag_id, transaction_amount, item_description)
+          VALUES (#{@merchant_id}, #{@tag_id}, #{@transaction_amount}, '#{@item_description}')
+          RETURNING id;")
+    @id = SqlRunner.run(sql)
+  end
+
+end
