f17fd236609339b588ca86522a92751ef054fff7
Colin McKean <colinmckeandigital@gmail.com>
2017-02-05 20:17:08 +0000

    correct rest routes

--- controllers/transactions_controller.rb
+++ controllers/transactions_controller.rb
@@ -6,20 +6,32 @@ require_relative '../models/Merchant'
 require_relative '../models/Transaction'
 
 get '/transactions/all' do
+  @total_spend = Transaction.total_spend()
   @transactions = Transaction.all()
   erb :"/transactions/index"
 end
 
 get '/transactions/new' do
+  @tags = Tag.all
+  @merchants = Merchant.all
  erb :"transactions/new"
 end
 
+post '/transactions/new' do
+  @transaction = Transaction.new(params)
+  @transaction.save
+  redirect to("/transactions/all")
+end
+
 get "/transactions/:id" do
+  @more_details = Transaction.more_details(params[:id])
   @transaction = Transaction.find(params[:id])
   erb :"transactions/show"
 end
 
 get '/transactions/:id/edit' do
+  @tags = Tag.all
+  @merchants = Merchant.all
   @transaction = Transaction.find(params[:id])
   erb :"transactions/edit"
 end
