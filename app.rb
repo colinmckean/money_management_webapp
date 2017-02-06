83ce7b9aeab11360870aebb52c15889585a2edcf
Colin McKean <colinmckeandigital@gmail.com>
2017-02-06 13:49:07 +0000

    add total spend to index route

--- app.rb
+++ app.rb
@@ -6,5 +6,6 @@ require_relative './controllers/tags_controller'
 require_relative './controllers/transactions_controller'
 
 get '/' do
+  @total_spend = Transaction.total_spend()
   erb (:index)
 end
