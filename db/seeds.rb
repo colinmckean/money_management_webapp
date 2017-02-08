8afd2a8a4e624d7621c7c2b896d5f71045c3d54c
Colin McKean <colinmckeandigital@gmail.com>
2017-02-08 11:35:39 +0000

    finished seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ require_relative '../models/Tag'
 require_relative '../models/Merchant'
 require_relative '../models/Transaction'
 require 'pry-byebug'
+
 merchant_1 = Merchant.new({
   'merchant_name' => 'Tesco'
   })
@@ -20,6 +21,9 @@ merchant_5 = Merchant.new({
 merchant_6 = Merchant.new({
   'merchant_name' => 'Burton'
   })
+merchant_7 = Merchant.new({
+  'merchant_name' => 'Local Pub'
+  })
 
 tag_1 = Tag.new({
   'tag_name' => 'Food'
@@ -34,7 +38,10 @@ tag_4 = Tag.new({
   'tag_name' => 'Lunch'
   })
 tag_5 = Tag.new({
-  'tag_name' => 'Credit Card payment'
+  'tag_name' => 'Finance'
+  })
+tag_6 = Tag.new({
+  'tag_name' => 'Booze'
   })
 
 merchant_1.save
@@ -43,14 +50,24 @@ merchant_3.save
 merchant_4.save
 merchant_5.save
 merchant_6.save
+merchant_7.save
 
 tag_1.save()
 tag_2.save()
 tag_3.save()
 tag_4.save()
 tag_5.save()
+tag_6.save()
 
-transactions_array = [transaction_1 = Transaction.new({
+transactions_array = [
+  transaction_1 = Transaction.new({
+  "merchant_id"=> "1",
+  "tag_id" => "1",
+  "transaction_amount" => "10.35",
+  "item_description" => "bought some christmas stuff.",
+  "transaction_date" => "20161201"
+  }),
+  transaction_1 = Transaction.new({
   "merchant_id"=> "1",
   "tag_id" => "1",
   "transaction_amount" => "5.35",
@@ -76,76 +93,41 @@ transaction_4 = Transaction.new({
   "tag_id" => "2",
   "transaction_amount" => "16.00",
   "item_description" => "Bought new shirt for work.",
-  "transaction_date" => "20170205"
+  "transaction_date" => "20170119"
   }),
-transaction_1 = Transaction.new({
+transaction_5 = Transaction.new({
   "merchant_id"=> "1",
   "tag_id" => "1",
-  "transaction_amount" => "8.29",
-  "item_description" => "bought Weekly Shopping. Bread, milk eggs.",
-  "transaction_date" => "20170206"
+  "transaction_amount" => "18.29",
+  "item_description" => "More shoping bought cakes.",
+  "transaction_date" => "20170120"
   }),
-transaction_10 = Transaction.new({
-  "merchant_id"=> "1",
-  "tag_id" => "1",
-  "transaction_amount" => "20.35",
-  "item_description" => "bought helicopter",
+transaction_6 = Transaction.new({
+  "merchant_id"=> "3",
+  "tag_id" => "5",
+  "transaction_amount" => "120.35",
+  "item_description" => "Loan payments",
+  "transaction_date" => "20160201"
+  }),
+transaction_7 = Transaction.new({
+  "merchant_id"=> "5",
+  "tag_id" => "3",
+  "transaction_amount" => "40.01",
+  "item_description" => "Filled Tank",
   "transaction_date" => "20170202"
   }),
-transaction_11 = Transaction.new({
-  "merchant_id"=> "1",
-  "tag_id" => "1",
-  "transaction_amount" => "20.35",
-  "item_description" => "bought helicopter",
+transaction_8 = Transaction.new({
+  "merchant_id"=> "4",
+  "tag_id" => "2",
+  "transaction_amount" => "299.99",
+  "item_description" => "bought new suit for interviews.",
   "transaction_date" => "20170202"
   }),
-transaction_12 = Transaction.new({
-  "merchant_id"=> "1",
-  "tag_id" => "1",
-  "transaction_amount" => "20.35",
-  "item_description" => "bought helicopter",
-  "transaction_date" => "20170202"
-  }),
-transaction_13 = Transaction.new({
-  "merchant_id"=> "1",
-  "tag_id" => "1",
-  "transaction_amount" => "20.35",
-  "item_description" => "bought helicopter",
-  "transaction_date" => "20170202"
-  }),
-transaction_14 = Transaction.new({
-  "merchant_id"=> "1",
-  "tag_id" => "1",
-  "transaction_amount" => "20.35",
-  "item_description" => "bought helicopter",
-  "transaction_date" => "20170202"
-  }),
-transaction_15 = Transaction.new({
-  "merchant_id"=> "1",
-  "tag_id" => "1",
-  "transaction_amount" => "20.35",
-  "item_description" => "bought helicopter",
-  "transaction_date" => "20170202"
-  }),
-transaction_16 = Transaction.new({
-  "merchant_id"=> "1",
-  "tag_id" => "1",
-  "transaction_amount" => "20.35",
-  "item_description" => "bought helicopter",
-  "transaction_date" => "20170202"
-  }),
-transaction_17 = Transaction.new({
-  "merchant_id"=> "1",
-  "tag_id" => "1",
-  "transaction_amount" => "20.35",
-  "item_description" => "bought helicopter",
-  "transaction_date" => "20170202"
-  }),
-transaction_18 = Transaction.new({
-  "merchant_id"=> "1",
-  "tag_id" => "1",
-  "transaction_amount" => "20.35",
-  "item_description" => "bought helicopter",
+transaction_9 = Transaction.new({
+  "merchant_id"=> "6",
+  "tag_id" => "2",
+  "transaction_amount" => "59.99",
+  "item_description" => "bought new shoes",
   "transaction_date" => "20170202"
   })
 ]
