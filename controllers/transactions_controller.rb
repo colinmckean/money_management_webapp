f512bfc5316616fc34617fbbadb09afc2e7bd777
Colin McKean <colinmckeandigital@gmail.com>
2017-02-09 23:14:50 +0000

    apply consistant syntax

--- controllers/transactions_controller.rb
+++ controllers/transactions_controller.rb
@@ -8,32 +8,32 @@ require_relative '../models/Transaction'
 get '/transactions/all' do
   @total_spend = Transaction.total_spend()
   @transactions = Transaction.all()
-  erb :"/transactions/index"
+  erb :'/transactions/index'
 end
 
 post '/transactions/' do
   @transactions = Transaction.by_month(params[:month],params[:year])
   @total_by_month = Transaction.total_by_month(params[:month], params[:year])
-  erb :"transactions/index"
+  erb :'transactions/index'
 end
 
 get '/transactions/new' do
   @tags = Tag.all
   @merchants = Merchant.all
- erb :"transactions/new"
+  erb :'transactions/new'
 end
 
 
 post '/transactions/new' do
   @transaction = Transaction.new(params)
   @transaction.save
-  redirect to("/transactions/all")
+  redirect to '/transactions/all'
 end
 
-get "/transactions/:id" do
+get '/transactions/:id' do
   @more_details = Transaction.more_details(params[:id])
   @transaction = Transaction.find(params[:id])
-  erb :"transactions/show"
+  erb :'transactions/show'
 end
 
 get '/transactions/:id/edit' do
@@ -45,10 +45,10 @@ end
 
 post '/transactions/:id' do
   @transaction = Transaction.update(params)
-  redirect to ("/transactions/#{params[:id]}")
+  redirect to "/transactions/#{params[:id]}"
 end
 
 post '/transactions/:id/delete' do
-    Transaction.delete(params[:id])
-    redirect to('/transactions/all')
+  Transaction.delete(params[:id])
+  redirect to '/transactions/all'
 end
