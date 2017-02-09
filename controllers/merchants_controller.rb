19e50043619fd81f79d0f5e5da95c2e8cceda120
Colin McKean <colinmckeandigital@gmail.com>
2017-02-09 23:13:23 +0000

    corrected syntax

--- controllers/merchants_controller.rb
+++ controllers/merchants_controller.rb
@@ -7,37 +7,37 @@ require_relative '../models/Transaction'
 
 get '/merchants/all' do
   @merchants = Merchant.all()
-  erb :"/merchants/index"
+  erb :'/merchants/index'
 end
 
 get '/merchants/new' do
-  erb :"merchants/new"
+  erb :'merchants/new'
 end
 
 post '/merchants/new' do
   @merchant = Merchant.new(params)
   @merchant.save
-  redirect to("/merchants/all")
+  redirect to '/merchants/all'
 end
 
-get "/merchants/:id" do
+get '/merchants/:id' do
   @total_spend = Merchant.total_spend(params[:id])
   @merchant = Merchant.find(params[:id])
-  @count = Transaction.count_tags(params[:id])
-  erb :"merchants/show"
+  @count = Transaction.count_merchants(params[:id])
+  erb :'merchants/show'
 end
 
 get '/merchants/:id/edit' do
   @merchant = Merchant.find(params[:id])
-  erb :"merchants/edit"
+  erb :'merchants/edit'
 end
 
-post "/merchants/:id" do
+post '/merchants/:id' do
   @merchant = Merchant.update(params)
-  redirect to ("/merchants/#{params[:id]}")
+  redirect to "/merchants/#{params[:id]}"
 end
 
-post "/merchants/:id/delete" do
+post '/merchants/:id/delete' do
   Merchant.delete(params[:id])
-  redirect to('/merchants/all')
+  redirect to '/merchants/all'
 end
